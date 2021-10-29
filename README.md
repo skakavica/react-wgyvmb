9877722f037ed35570ab49a583b2a2c1e6d16399
skakavica <21519939@student.uwl.ac.uk>
2021-10-29 20:02:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wgyvmb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wgyvmb)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Lana Del Rey!</h1>
+      <p>She is the bestest :)</p>
+    </div>
+  );
+}
